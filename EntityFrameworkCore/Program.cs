ed42d60f25fc86d26f433be7a09ec63f7957d69d
emirhan erbil <emirhan117net@gmail.com>
2021-12-06 11:37:11 +0300

    d

--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -6,7 +6,7 @@ namespace EntityFrameworkCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldr!");
         }
     }
 }
