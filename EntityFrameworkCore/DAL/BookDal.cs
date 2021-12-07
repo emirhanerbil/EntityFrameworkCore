fe41f565e5531eb23e4f16b1fb6ab7de4fe0ef55
emirhan erbil <emirhan117net@gmail.com>
2021-12-07 06:00:29 +0300

    lab

---
+++ EntityFrameworkCore/DAL/BookDal.cs
@@ -0,0 +1,27 @@
+﻿using EntityFrameworkCore.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.DAL
+{
+    class BookDal
+    {
+        public void AddBook()
+        {
+            var context = new DBContext();
+
+            var book = new Book()
+            {
+                BookId = 1,
+                CreatedDate = DateTime.Now,
+                Title = "Beyaz Diş"
+            };
+            context.Add(book);
+            context.SaveChanges();
+
+        }
+    }
+}
--- EntityFrameworkCore/Program.cs
+++ EntityFrameworkCore/Program.cs
@@ -6,7 +6,8 @@ namespace EntityFrameworkCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Worldr!");
+            
+             
         }
     }
 }
